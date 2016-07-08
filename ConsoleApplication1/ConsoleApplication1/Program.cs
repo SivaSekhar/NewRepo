2e9e608bd0f76018e4a9fd3fb09911de4f52ce5f
SivaSekhar <v-siperi@microsoft.com>
2016-07-08 12:39:59 +0530

    Update Program.cs

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             
-            Console.WriteLine("dev git");
+            Console.WriteLine("dev git 1");
         }
     }
 }
