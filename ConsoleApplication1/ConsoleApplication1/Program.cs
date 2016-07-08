1ab6ccc5f6865099f61493af86809c129945fffd
v-siperi <v-siperi@HYDPCM390954D>
2016-07-08 12:36:15 +0530

    commited in feature1

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("dev git ");
+            
             Console.WriteLine("feature1 git");
         }
     }
