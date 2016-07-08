0fefe3dd9b682686930751ace10aa74f89b05189
v-siperi <v-siperi@HYDPCM390954D>
2016-07-08 12:26:07 +0530

    commit

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("program in master branch git 2");
+            Console.WriteLine("program in master branch git 1");
         }
     }
 }
