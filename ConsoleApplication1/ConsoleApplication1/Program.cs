d62e4bec0f012b6aa4e419eae037f90002e4cc00
v-siperi <v-siperi@HYDPCM390954D>
2016-07-08 12:39:19 +0530

    commited

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             
-            Console.WriteLine("feature1 branch git");
+            Console.WriteLine("dev git");
         }
     }
 }
